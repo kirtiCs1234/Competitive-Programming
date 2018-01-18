bfe8764726094c77d15a9768a13dea05830f4564
kirtiCs1234 <32388215+kirtiCs1234@users.noreply.github.com>
2018-01-18 14:08:31 +0530

    Update ATOM.cpp

--- ATOM.cpp
+++ ATOM.cpp
@@ -1,5 +1,5 @@
-#include<cstdio>
-
+#include<iostream>
+using namespace std;
 double n,k,m;
 
 void solve()
@@ -12,21 +12,21 @@ void solve()
         k = r*k;
         ans++;
     }
-    //cout << ans << endl;
-    printf("%d\n",ans);
+    cout << ans << endl;
+    //printf("%d\n",ans);
 }
 
 int main()
 {
-//    ios::sync_with_stdio(false);
+   ios::sync_with_stdio(false);
 
     int t;
-    //cin >> t;
+    cin >> t;
     scanf("%d",&t);
     while(t--)
     {
-        //cin >> n >> k >> m;
-        scanf("%lf%lf%lf",&n,&k,&m);
+        cin >> n >> k >> m;
+        //scanf("%lf%lf%lf",&n,&k,&m);
         solve();
     }
     return 0;
